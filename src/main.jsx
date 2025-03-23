12911f153203ff58a2438c47ca746349e79a4906
Carlos-mtzg <carlos.mtzg2004@gmail.com>
2025-03-23 01:32:08 -0600

    feat(router): :zap: implement react-router-dom with basic routes for home and 404 error page

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,31 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
+
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import Error404 from './pages/errors/Error404.jsx'
 import App from './App.jsx'
+
+const router = createBrowserRouter(
+  [
+    {
+      path: '/',
+      children: [
+        {
+          index: true,
+          element: <App />
+        },
+        {
+          path: '*',
+          element: <Error404 />
+        }
+      ]
+    }
+  ]
+);
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router}></RouterProvider>
   </StrictMode>,
 )
