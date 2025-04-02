47b6f8c99eac445151d15980fe999a82e69d4bed
AnettVera <20223tn034@utez.edu.mx>
2025-04-02 16:06:34 -0600

    fix: :sparkles: add the route to view entries

--- src/main.jsx
+++ src/main.jsx
@@ -16,6 +16,7 @@ import PrivateRoute from './components/PrivateRoute.jsx';
 import PublicRoute from './components/PublicRoute.jsx';
 import ResetPassword from './pages/auth/ResetPassword.jsx';
 import ProductEntry from './pages/user/ProductEntry.jsx';
+import Entries from './pages/admin/Entries.jsx';
 
 const router = createBrowserRouter([
   {
@@ -23,8 +24,16 @@ const router = createBrowserRouter([
     element: <PublicRoute />,
     children: [
       { path: 'login', element: <Login />, errorElement: <Error500 /> },
-      { path: 'forgot-password', element: <ForgotPassword />, errorElement: <Error500 /> },
-      { path: 'reset-password/:token', element: <ResetPassword />, errorElement: <Error500 /> },
+      {
+        path: 'forgot-password',
+        element: <ForgotPassword />,
+        errorElement: <Error500 />,
+      },
+      {
+        path: 'reset-password/:token',
+        element: <ResetPassword />,
+        errorElement: <Error500 />,
+      },
     ],
   },
   {
@@ -36,7 +45,12 @@ const router = createBrowserRouter([
         element: <Layout />,
         children: [
           { index: true, element: <Home />, errorElement: <Error500 /> },
-          { path: 'productEntries', element: <ProductEntry />, errorElement: <Error500 /> },
+          {
+            path: 'productEntries',
+            element: <ProductEntry />,
+            errorElement: <Error500 />,
+          },
+          { path: 'entries', element: <Entries />, errorElement: <Error500 /> },
         ],
       },
     ],
