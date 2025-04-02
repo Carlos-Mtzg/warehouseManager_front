5b5f70f68e6b63fbb8512ab25afa844468eba8a9
Carlos-mtzg <carlos.mtzg2004@gmail.com>
2025-04-02 15:34:04 -0600

    feat(auth): :zap: add activateAccount function to handle user account activation via API

--- src/services/ApiAuth.jsx
+++ src/services/ApiAuth.jsx
@@ -3,47 +3,66 @@ import axios from 'axios';
 const API_URL = import.meta.env.VITE_API_URL_LOCAL;
 
 export async function authLogin(request) {
-    let datos = axios
-        .post(`${API_URL}auth/login`, request, {
-            headers: { 'content-type': 'application/json' }
-        })
-        .then((response) => {
-            return response.data;
-        }).catch((error) => {
-            return { 'state': 'error' }
-        });
-    return datos;
+  let datos = axios
+    .post(`${API_URL}auth/login`, request, {
+      headers: { 'content-type': 'application/json' },
+    })
+    .then((response) => {
+      return response.data;
+    })
+    .catch(() => {
+      return { state: 'error' };
+    });
+  return datos;
 }
 
 export async function resetPasswordEmail(email) {
-    try {
-        const response = await axios.post(
-            `${API_URL}auth/reset-email`,
-            { email },
-            { headers: { "Content-Type": "application/json" } }
-        );
-        return response.data;
-    } catch (error) {
-        return {
-            state: "error",
-            message: error.response?.data?.message || "Error al enviar el correo",
-        };
-    }
+  try {
+    const response = await axios.post(
+      `${API_URL}auth/reset-email`,
+      { email },
+      { headers: { 'Content-Type': 'application/json' } }
+    );
+    return response.data;
+  } catch (error) {
+    return {
+      state: 'error',
+      message: error.response?.data?.message || 'Error al enviar el correo',
+    };
+  }
 }
 
 export async function resetPassword(token, password) {
-    try {
-        const response = await axios.post(
-            `${API_URL}auth/reset-password/${token}`,
-            { password },
-            { headers: { "Content-Type": "application/json" } }
-        );
-        return response.data;
-    } catch (error) {
-        return {
-            state: "error",
-            message: error.response?.data?.message || "Error al restablecer la contraseña",
-        };
-    }
+  try {
+    const response = await axios.post(
+      `${API_URL}auth/reset-password/${token}`,
+      { password },
+      { headers: { 'Content-Type': 'application/json' } }
+    );
+    return response.data;
+  } catch (error) {
+    return {
+      state: 'error',
+      message:
+        error.response?.data?.message || 'Error al restablecer la contraseña',
+    };
+  }
 }
 
+export async function activateAccount(token, password) {
+  try {
+    const response = await axios.post(
+      `${API_URL}auth/activate-account/${token}`,
+      { password },
+      { headers: { 'Content-Type': 'application/json' } }
+    );
+    return response.data;
+  } catch (error) {
+    return {
+      state: 'error',
+      message:
+        error.response?.data?.message ||
+        'Error al activar tu cuenta, ponte en contacto con al administrador',
+    };
+  }
+}
