f3a18fd44e023849755280e31c158f003fa57772
Carlos-mtzg <carlos.mtzg2004@gmail.com>
2025-04-06 17:28:37 -0600

    chore(dependencies): :arrow_up: import Bootstrap JS bundle in main.jsx
    This ensures interactive Bootstrap elements work as expected throughout the application.

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,7 @@ import { AuthProvider } from './context/AuthProvider.jsx';
 import { createRoot } from 'react-dom/client';
 import { StrictMode } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 import 'bootstrap-icons/font/bootstrap-icons.css';
 import './index.css';
 
