e660eb856567d5821eb33c0bcd369ac85092708c
Carlos-mtzg <carlos.mtzg2004@gmail.com>
2025-04-06 19:59:25 -0600

    feat(users): :zap: add SweetAlert for user registration and update user list after adding a new user in AddUserModal

--- src/components/AddUserModal.jsx
+++ src/components/AddUserModal.jsx
@@ -5,9 +5,9 @@ import { Modal } from 'react-bootstrap';
 import styles from "../assets/css/users.module.css"
 import { useState } from 'react';
 import { registerUser } from '../services/ApiUser';
-import toast, { Toaster } from 'react-hot-toast';
+import Swal from 'sweetalert2';
 
-const AddUserModal = ({ show, handleClose, onResetForm }) => {
+const AddUserModal = ({ show, handleClose, onResetForm, onUserAdded}) => {
     const REQUIRED_FIELDS = 'Campo obligatorio';
     const [isSubmitting, setIsSubmitting] = useState(false);
 
@@ -71,15 +71,16 @@ const AddUserModal = ({ show, handleClose, onResetForm }) => {
 
                 const response = await registerUser(requestBody.name, requestBody.lastname, requestBody.email, requestBody.role);
                 if (response.state === 'success') {
-                    toast.success('Usuario registrado correctamente');
+                    Swal.fire('Registro correcto', 'Usuario registrado correctamente', 'success');
                     resetForm();
                     handleClose();
+                    if (onUserAdded) onUserAdded();
                 } else {
-                    toast.error(response.message);
+                    Swal.fire('Error', 'Ocurrió un error inesperado', 'error');
                 }
                 setIsSubmitting(false);
             } catch (error) {
-                toast.error('Ocurrió un error inesperado');
+                Swal.fire('Error', 'Ocurrió un error inesperado', 'error');
             } finally {
                 await new Promise((resolve) => setTimeout(resolve, 2000));
             }
@@ -93,154 +94,151 @@ const AddUserModal = ({ show, handleClose, onResetForm }) => {
     };
 
     return (
-        <>
-            <Toaster position="top-center" reverseOrder={false} />
-            <Modal show={show} onHide={handleClose} centered backdrop="static" keyboard={false}>
-                <Modal.Header className={`modal-title fs-5 ${styles['modal-header']}`}>
-                    <h3>Agregar Usuario</h3>
-                </Modal.Header>
-                <form onSubmit={handleSubmit}>
-                    <Modal.Body className='p-4 rounded'>
-                        <div className="d-flex flex-column gap-3">
-                            <div className="form-group">
-                                <label
-                                    htmlFor="name"
-                                    className={`form-label fw-semibold ${styles['label']}`}
-                                >
-                                    Nombre(s):
-                                </label>
-                                <input
-                                    type="text"
-                                    id="name"
-                                    name="name"
-                                    value={values.name}
-                                    onChange={handleChange}
-                                    onBlur={handleBlur}
-                                    className={`form-control py-3 ${touched.name && errors.name ? 'is-invalid' : ''}`}
-                                    placeholder="Escribe aquí tu nueva contraseña"
-                                />
-                                {touched.name && errors.name ? (
-                                    <div className="text-danger mt-1" style={{ fontSize: '15px' }}>
-                                        {errors.name}
-                                    </div>
-                                ) : null}
-                            </div>
-                            <div className="form-group">
-                                <label
-                                    htmlFor="lastname"
-                                    className={`form-label fw-semibold ${styles['label']}`}
-                                >
-                                    Apellidos(s):
-                                </label>
-                                <input
-                                    type="text"
-                                    id="lastname"
-                                    name="lastname"
-                                    value={values.lastname}
-                                    onChange={handleChange}
-                                    onBlur={handleBlur}
-                                    className={`form-control py-3 ${touched.lastname && errors.lastname ? 'is-invalid' : ''}`}
-                                    placeholder="Escribe aquí tu nueva contraseña"
-                                />
-                                {touched.lastname && errors.lastname ? (
-                                    <div className="text-danger mt-1" style={{ fontSize: '15px' }}>
-                                        {errors.lastname}
-                                    </div>
-                                ) : null}
-                            </div>
-                            <div className="form-group">
-                                <label
-                                    htmlFor="email"
-                                    className={`form-label fw-semibold ${styles['label']}`}
-                                >
-                                    Correo Electrónico:
-                                </label>
-                                <input
-                                    type="email"
-                                    id="email"
-                                    name="email"
-                                    value={values.email}
-                                    onChange={handleChange}
-                                    onBlur={handleBlur}
-                                    className={`form-control py-3 ${touched.email && errors.email ? 'is-invalid' : ''}`}
-                                    placeholder="Escribe aquí tu nueva contraseña"
-                                />
-                                {touched.email && errors.email ? (
-                                    <div className="text-danger mt-1" style={{ fontSize: '15px' }}>
-                                        {errors.email}
-                                    </div>
-                                ) : null}
-                            </div>
-                            <div className="form-group">
-                                <label
-                                    htmlFor="role"
-                                    className={`form-label fw-semibold ${styles['label']}`}
-                                >
-                                    Rol de Usuario:
-                                </label>
-                                <select
-                                    id="role"
-                                    name="role"
-                                    value={values.role}
-                                    onChange={handleChange}
-                                    onBlur={handleBlur}
-                                    className={`form-control py-3 ${touched.role && errors.role ? 'is-invalid' : ''}`}
-                                >
-                                    <option value="" disabled>
-                                        Selecciona una opción
-                                    </option>
-                                    <option value="admin">Administrador</option>
-                                    <option value="user">Usuario</option>
-                                </select>
-                                {touched.role && errors.role ? (
-                                    <div className="text-danger mt-1" style={{ fontSize: '15px' }}>
-                                        {errors.role}
-                                    </div>
-                                ) : null}
-                            </div>
+        <Modal show={show} onHide={handleClose} centered backdrop="static" keyboard={false}>
+            <Modal.Header className={`modal-title fs-5 ${styles['modal-header']}`}>
+                <h3>Agregar Usuario</h3>
+            </Modal.Header>
+            <form onSubmit={handleSubmit}>
+                <Modal.Body className='p-4 rounded'>
+                    <div className="d-flex flex-column gap-3">
+                        <div className="form-group">
+                            <label
+                                htmlFor="name"
+                                className={`form-label fw-semibold ${styles['label']}`}
+                            >
+                                Nombre(s):
+                            </label>
+                            <input
+                                type="text"
+                                id="name"
+                                name="name"
+                                value={values.name}
+                                onChange={handleChange}
+                                onBlur={handleBlur}
+                                className={`form-control py-3 ${touched.name && errors.name ? 'is-invalid' : ''}`}
+                                placeholder="Escribe aquí tu nueva contraseña"
+                            />
+                            {touched.name && errors.name ? (
+                                <div className="text-danger mt-1" style={{ fontSize: '15px' }}>
+                                    {errors.name}
+                                </div>
+                            ) : null}
                         </div>
-                    </Modal.Body>
-                    <Modal.Footer>
+                        <div className="form-group">
+                            <label
+                                htmlFor="lastname"
+                                className={`form-label fw-semibold ${styles['label']}`}
+                            >
+                                Apellidos(s):
+                            </label>
+                            <input
+                                type="text"
+                                id="lastname"
+                                name="lastname"
+                                value={values.lastname}
+                                onChange={handleChange}
+                                onBlur={handleBlur}
+                                className={`form-control py-3 ${touched.lastname && errors.lastname ? 'is-invalid' : ''}`}
+                                placeholder="Escribe aquí tu nueva contraseña"
+                            />
+                            {touched.lastname && errors.lastname ? (
+                                <div className="text-danger mt-1" style={{ fontSize: '15px' }}>
+                                    {errors.lastname}
+                                </div>
+                            ) : null}
+                        </div>
+                        <div className="form-group">
+                            <label
+                                htmlFor="email"
+                                className={`form-label fw-semibold ${styles['label']}`}
+                            >
+                                Correo Electrónico:
+                            </label>
+                            <input
+                                type="email"
+                                id="email"
+                                name="email"
+                                value={values.email}
+                                onChange={handleChange}
+                                onBlur={handleBlur}
+                                className={`form-control py-3 ${touched.email && errors.email ? 'is-invalid' : ''}`}
+                                placeholder="Escribe aquí tu nueva contraseña"
+                            />
+                            {touched.email && errors.email ? (
+                                <div className="text-danger mt-1" style={{ fontSize: '15px' }}>
+                                    {errors.email}
+                                </div>
+                            ) : null}
+                        </div>
+                        <div className="form-group">
+                            <label
+                                htmlFor="role"
+                                className={`form-label fw-semibold ${styles['label']}`}
+                            >
+                                Rol de Usuario:
+                            </label>
+                            <select
+                                id="role"
+                                name="role"
+                                value={values.role}
+                                onChange={handleChange}
+                                onBlur={handleBlur}
+                                className={`form-control py-3 ${touched.role && errors.role ? 'is-invalid' : ''}`}
+                            >
+                                <option value="" disabled>
+                                    Selecciona una opción
+                                </option>
+                                <option value="admin">Administrador</option>
+                                <option value="user">Usuario</option>
+                            </select>
+                            {touched.role && errors.role ? (
+                                <div className="text-danger mt-1" style={{ fontSize: '15px' }}>
+                                    {errors.role}
+                                </div>
+                            ) : null}
+                        </div>
+                    </div>
+                </Modal.Body>
+                <Modal.Footer>
+                    <button
+                        className={`rounded ${styles['secondary-btn']}`} onClick={handleCancel} type='button'>
+                        <div className={`btn d-flex text-center ${styles['secondary-content']}`}>
+                            Cancelar
+                        </div>
+                        <span></span>
+                    </button>
+                    {isSubmitting ? (
                         <button
-                            className={`rounded ${styles['secondary-btn']}`} onClick={handleCancel} type='button'>
-                            <div className={`btn d-flex text-center ${styles['secondary-content']}`}>
-                                Cancelar
+                            className={`rounded ${styles['primary-btn']}`}
+                            type="submit"
+                            disabled
+                        >
+                            <div className={`d-flex align-items-center px-2 gap-2 ${styles['primary-content']}`} style={{ height: '37.6px' }}>
+                                Cargando
+                                <output
+                                    className="spinner-border"
+                                    style={{ height: "1.2rem", width: "1.2rem", fontSize: "10px" }}
+                                >
+                                    <span className="visually-hidden"></span>
+                                </output>
                             </div>
                             <span></span>
                         </button>
-                        {isSubmitting ? (
-                            <button
-                                className={`rounded ${styles['primary-btn']}`}
-                                type="submit"
-                                disabled
-                            >
-                                <div className={`d-flex align-items-center px-2 gap-2 ${styles['primary-content']}`} style={{ height: '37.6px' }}>
-                                    Cargando
-                                    <output
-                                        className="spinner-border"
-                                        style={{ height: "1.2rem", width: "1.2rem", fontSize: "10px" }}
-                                    >
-                                        <span className="visually-hidden"></span>
-                                    </output>
-                                </div>
-                                <span></span>
-                            </button>
-                        ) : (
-                            <button
-                                className={`rounded ${styles['primary-btn']}`}
-                                type='submit'
-                                disabled={isSubmitting}
-                            >
-                                <div className={`btn d-flex text-center ${styles['primary-content']}`}>
-                                    Confirmar
-                                </div>
-                                <span></span>
-                            </button>
-                        )}
-                    </Modal.Footer>
-                </form>
-            </Modal>
-        </>
+                    ) : (
+                        <button
+                            className={`rounded ${styles['primary-btn']}`}
+                            type='submit'
+                            disabled={isSubmitting}
+                        >
+                            <div className={`btn d-flex text-center ${styles['primary-content']}`}>
+                                Confirmar
+                            </div>
+                            <span></span>
+                        </button>
+                    )}
+                </Modal.Footer>
+            </form>
+        </Modal>
     )
 };
 
@@ -248,6 +246,7 @@ AddUserModal.propTypes = {
     show: PropTypes.bool.isRequired,
     handleClose: PropTypes.func.isRequired,
     onResetForm: PropTypes.func.isRequired,
+    onUserAdded: PropTypes.func,
 };
 
 export default AddUserModal;
