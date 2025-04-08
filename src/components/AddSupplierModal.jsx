c8a927eb64665c39ebbc060d545732b25226e8b1
Carlos-mtzg <carlos.mtzg2004@gmail.com>
2025-04-08 09:57:47 -0600

    feat(AddSupplierModal): :sparkles: Refactor supplier registration form with validation and improved UI

--- src/components/AddSupplierModal.jsx
+++ src/components/AddSupplierModal.jsx
@@ -1,118 +1,170 @@
-import React, {useState} from "react";
-import {Modal, Button, Form} from "react-bootstrap";
-import "bootstrap/dist/css/bootstrap.min.css";
-import AxiosClient from "../config/axios-client";
+import PropTypes from 'prop-types';
 import Swal from "sweetalert2";
+import React, { useState } from "react";
+import { Modal } from "react-bootstrap";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { addSupplierSchema } from "../validations/entriesValidation";
+import { useFormik } from "formik";
+import { createSupplier } from "../services/ApiEntries";
+import styles from '../assets/css/entries.module.css'
 
-const AddSupplierModal = ({show, handleClose, onSupplierAdded}) => {
-    const [name, setName] = useState("");
-    const [email, setEmail] = useState("");
+const AddSupplierModal = ({ show, handleClose }) => {
+    const [isSubmitting, setIsSubmitting] = useState(false);
 
-    const handleRegister = async () => {
-        const requestBody = {
-            name,
-            email,
-        };
+    const validationSchema = addSupplierSchema;
 
-        try {
-            const token = localStorage.getItem("accessToken"); // Retrieve the token if needed
-            const response = await AxiosClient.post("supplier/", requestBody, {
-                headers: {
-                    Authorization: `Bearer ${token}`, // Add the token to the request headers
-                },
-            });
-            console.log("Proveedor registrado exitosamente:", response);
-            await Swal.fire({
-                icon: "success",
-                title: "Proveedor registrado exitosamente.",
-                showConfirmButton: false,
-                timer: 1500,
-            });
-            handleClose(); // Close the modal after successful registration
-            if (onSupplierAdded) onSupplierAdded(); // Refresh the supplier list
-
-        } catch (error) {
-            console.error("Error al registrar el proveedor:", error);
-            alert("Error al registrar el proveedor. Por favor, inténtalo de nuevo.");
+    const {
+        handleSubmit,
+        handleChange,
+        handleBlur,
+        values,
+        errors,
+        touched,
+        resetForm
+    } = useFormik({
+        initialValues: {
+            name: "",
+            email: ""
+        },
+        validationSchema,
+        onSubmit: async (values) => {
+            try {
+                setIsSubmitting(true)
+                const response = await createSupplier(values.name, values.email);
+                if (response.state === 'success') {
+                    resetForm();
+                    handleClose();
+                    Swal.fire({
+                        title: 'Registro correcto',
+                        text: 'Proveedor registrado correctamente',
+                        icon: 'success',
+                        showConfirmButton: false,
+                        timer: 2000
+                    });
+                }
+                setIsSubmitting(false)
+            } catch (error) {
+                Swal.fire({
+                    title: 'Error',
+                    text: 'Ocurrió un error inesperado',
+                    icon: 'error',
+                    showConfirmButton: false,
+                    timer: 2000
+                })
+            } finally {
+                await new Promise((resolve) => setTimeout(resolve, 2000));
+            }
         }
+    });
+
+    const handleCancel = () => {
+        resetForm();
+        handleClose();
     };
 
-
     return (
-        <Modal show={show} onHide={handleClose} centered>
-            <Modal.Body className="p-5 rounded" style={{backgroundColor: "#fff"}}>
-                <h4 className="text-center fw-bold mb-4" style={{color: "#1f3f38"}}>
-                    Agregar proveedor
-                </h4>
-
-                <Form.Group className="mb-3">
-                    <Form.Label className="fw-semibold" style={{color: "#1f3f38"}}>
-                        Nombre del proveedor o empresa:
-                    </Form.Label>
-                    <Form.Control
-                        type="text"
-                        placeholder="Nombre"
-                        value={name}
-                        onChange={(e) => setName(e.target.value)}
-                        style={{
-                            backgroundColor: "#f8f8f8",
-                            borderRadius: "8px",
-                            border: "1px solid #ddd",
-                            fontSize: "16px",
-                            padding: "10px",
-                        }}
-                    />
-                </Form.Group>
-
-                <Form.Group className="mb-4">
-                    <Form.Label className="fw-semibold" style={{color: "#1f3f38"}}>
-                        Correo electrónico del proveedor
-                    </Form.Label>
-                    <Form.Control
-                        type="email"
-                        placeholder="Email"
-                        value={email}
-                        onChange={(e) => setEmail(e.target.value)}
-                        style={{
-                            backgroundColor: "#f8f8f8",
-                            borderRadius: "8px",
-                            border: "1px solid #ddd",
-                            fontSize: "16px",
-                            padding: "10px",
-                        }}
-                    />
-                </Form.Group>
-
-                <div className="d-flex justify-content-between">
-                    <Button
-                        variant="outline-success"
-                        className="w-50 me-2 fw-semibold"
-                        style={{
-                            fontSize: "18px",
-                            borderColor: "#3d5c51",
-                            color: "#3d5c51",
-                            borderWidth: "2px",
-                        }}
-                        onClick={handleClose}
-                    >
-                        Cancelar
-                    </Button>
-
-                    <Button
-                        className="w-50 fw-semibold"
-                        style={{
-                            backgroundColor: "#1f3f38",
-                            borderColor: "#1f3f38",
-                            fontSize: "18px",
-                        }}
-                        onClick={handleRegister}
-                    >
-                        Registrar
-                    </Button>
-                </div>
-            </Modal.Body>
+        <Modal show={show} onHide={handleClose} centered backdrop="static" keyboard={false}>
+            <Modal.Header className={`modal-title fs-5 ${styles['modal-header']}`}>
+                <h3>Agregar Proveedor</h3>
+            </Modal.Header>
+            <form onSubmit={handleSubmit}>
+                <Modal.Body className="p-5 rounded">
+                    <div className="d-flex flex-column gap-3">
+                        <div className="form-group">
+                            <label
+                                htmlFor="name"
+                                className={`form-label fw-semibold ${styles['label']}`}
+                            >
+                                Proveedor:
+                            </label>
+                            <input
+                                type="text"
+                                id="name"
+                                name="name"
+                                value={values.name}
+                                onChange={handleChange}
+                                onBlur={handleBlur}
+                                className={`form-control py-3 ${touched.name && errors.name ? 'is-invalid' : ''}`}
+                                placeholder="Escribe aquí el nombre del proveedor"
+                            />
+                            {touched.name && errors.name ? (
+                                <div className="text-danger mt-1" style={{ fontSize: '15px' }}>
+                                    {errors.name}
+                                </div>
+                            ) : null}
+                        </div>
+                        <div className="form-group">
+                            <label
+                                htmlFor="email"
+                                className={`form-label fw-semibold ${styles['label']}`}
+                            >
+                                Correo Electrónico:
+                            </label>
+                            <input
+                                type="text"
+                                id="email"
+                                name="email"
+                                value={values.email}
+                                onChange={handleChange}
+                                onBlur={handleBlur}
+                                className={`form-control py-3 ${touched.email && errors.email ? 'is-invalid' : ''}`}
+                                placeholder="Escribe aquí el correo del proveedor"
+                            />
+                            {touched.email && errors.email ? (
+                                <div className="text-danger mt-1" style={{ fontSize: '15px' }}>
+                                    {errors.email}
+                                </div>
+                            ) : null}
+                        </div>
+                    </div>
+                </Modal.Body>
+                <Modal.Footer>
+                    <button
+                        className={`rounded ${styles['secondary-btn']}`} onClick={handleCancel} type='button'>
+                        <div className={`btn d-flex text-center ${styles['secondary-content']}`}>
+                            Cancelar
+                        </div>
+                        <span></span>
+                    </button>
+                    {isSubmitting ? (
+                        <button
+                            className={`rounded ${styles['primary-btn']}`}
+                            type="submit"
+                            disabled
+                        >
+                            <div className={`d-flex align-items-center px-2 gap-2 ${styles['primary-content']}`} style={{ height: '37.6px' }}>
+                                Cargando
+                                <output
+                                    className="spinner-border"
+                                    style={{ height: "1.2rem", width: "1.2rem", fontSize: "10px" }}
+                                >
+                                    <span className="visually-hidden"></span>
+                                </output>
+                            </div>
+                            <span></span>
+                        </button>
+                    ) : (
+                        <button
+                            className={`rounded ${styles['primary-btn']}`}
+                            type='submit'
+                            disabled={isSubmitting}
+                        >
+                            <div className={`btn d-flex text-center ${styles['primary-content']}`}>
+                                Confirmar
+                            </div>
+                            <span></span>
+                        </button>
+                    )}
+                </Modal.Footer>
+            </form>
         </Modal>
     );
+};
+
+
+AddSupplierModal.propTypes = {
+    show: PropTypes.bool.isRequired,
+    handleClose: PropTypes.func.isRequired,
 };
 
 export default AddSupplierModal;
