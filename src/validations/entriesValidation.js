c78547747565c2ef13d0ee11f5874de9a95a701b
Carlos-mtzg <carlos.mtzg2004@gmail.com>
2025-04-08 14:44:58 -0600

    fix(validations): :bug: Correct typo in required fields message and add product entries validation schema

--- src/validations/entriesValidation.js
+++ src/validations/entriesValidation.js
@@ -1,6 +1,6 @@
 import * as Yup from "yup";
 
-const REQUIRED_FIELDS = 'Este campo obligatorio';
+const REQUIRED_FIELDS = 'Este campo es obligatorio';
 const LETTERS_SPACES = 'Este campo solo puede contener letras y espacios';
 const NO_SPACES = 'Este campo no puede contener solo espacios';
 const INVALID_EMAIL = 'El correo electrónico no es válido';
@@ -67,3 +67,60 @@ export const addSupplierSchema = Yup.object({
         .notOneOf(['Script', 'script'], WORLDS_NOT_ALLOWED)
         .trim(NO_SPACES),
 })
+
+export const productEntriesSchema = Yup.object({
+    selectedSupplier: Yup.string()
+        .required(REQUIRED_FIELDS),
+    selectedCategory: Yup.string()
+        .required(REQUIRED_FIELDS),
+    productName: Yup.string()
+        .required(REQUIRED_FIELDS)
+        .matches(
+            /^[^\u3164\u200B\uFEFF]*$/,
+            INVISIBLE_CHARACTERS
+        )
+        .matches(
+            /^[a-zA-ZáéíóúÁÉÍÓÚñÑ\s]+$/,
+            LETTERS_SPACES
+        )
+        .min(3, 'El nombre debe tener al menos 3 caracteres')
+        .max(30, 'El nombre no puede exceder los 30 caracteres')
+        .matches(
+            /^[^<>]*$/,
+            CHARACTERS_NOT_ALLOWED
+        )
+        .notOneOf(['Script', 'script'], WORLDS_NOT_ALLOWED)
+        .matches(
+            /^\S+(?: \S+)*$/,
+            CONSECUTIVE_SPACES
+        )
+        .trim(NO_SPACES),
+    measurementUnit: Yup.string()
+        .required(REQUIRED_FIELDS)
+        .matches(
+            /^[^\u3164\u200B\uFEFF]*$/,
+            INVISIBLE_CHARACTERS
+        )
+        .matches(
+            /^[a-zA-ZáéíóúÁÉÍÓÚñÑ\s]+$/,
+            LETTERS_SPACES
+        )
+        .min(3, 'Este campo debe tener al menos 3 caracteres')
+        .max(30, 'Este campo no puede exceder los 30 caracteres')
+        .matches(
+            /^[^<>]*$/,
+            CHARACTERS_NOT_ALLOWED
+        )
+        .notOneOf(['Script', 'script'], WORLDS_NOT_ALLOWED)
+        .matches(
+            /^\S+(?: \S+)*$/,
+            CONSECUTIVE_SPACES
+        )
+        .trim(NO_SPACES),
+    quantity: Yup.string()
+        .required(REQUIRED_FIELDS)
+        .matches(/^\d+$/, 'Este campo solo puede contener números'),
+    unitPrice: Yup.string()
+        .required(REQUIRED_FIELDS)
+        .matches(/^\d+(\.\d{1,2})?$/, 'Este campo solo puede contener números y hasta dos decimales'),
+});
