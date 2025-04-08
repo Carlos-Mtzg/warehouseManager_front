45b90fae25c36216e04a9fbe4ce73fbe94cda6f7
Carlos-mtzg <carlos.mtzg2004@gmail.com>
2025-04-08 17:52:52 -0600

    fix(Login): :bug: Update navigation path to product entries page

--- src/pages/auth/Login.jsx
+++ src/pages/auth/Login.jsx
@@ -43,7 +43,7 @@ const Login = () => {
             showConfirmButton: false,
             timer: 1000
           }).then(() => {
-            navigate('/admin/product-entries');
+            navigate('/product-entries');
           });
         } else {
           Swal.fire({
