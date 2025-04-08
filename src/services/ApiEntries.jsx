05f1ef3d5d5bf1df5577887df38decd773767442
Carlos-mtzg <carlos.mtzg2004@gmail.com>
2025-04-08 01:18:44 -0600

    feat(api): :sparkles: Implement createCategory function for category creation with error handling

---
+++ src/services/ApiEntries.jsx
@@ -0,0 +1,32 @@
+import axios from 'axios';
+
+const API_URL = import.meta.env.VITE_API_URL_LOCAL;
+
+export async function createCategory(categoryName) {
+    try {
+        const token = localStorage.getItem('accessToken');
+        const response = await axios.post(
+            `${API_URL}category/`,
+            {
+                name: categoryName,
+            },
+            {
+                headers: {
+                    'Content-Type': 'application/json',
+                    Authorization: `Bearer ${token}`,
+                },
+            }
+        );
+        return {
+            state: 'success',
+            status: response.status,
+            message: response.data.message,
+        };
+    } catch (error) {
+        return {
+            state: 'error',
+            status: error.response?.status || 500,
+            message: error.response?.data?.message || 'Error al registrar la categoria',
+        };
+    }
+}
