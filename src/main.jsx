2bad6f2d1d97f6125d7c277a29373ab811986282
OliverVlqz <oliveratom772@gmail.com>
2025-04-09 10:20:32 -0600

    fix(main): corregido error de importaciónes en main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -7,15 +7,6 @@ import 'bootstrap/dist/js/bootstrap.bundle.min.js'
 import 'bootstrap-icons/font/bootstrap-icons.css'
 import '../src/assets/css/page-animations.css'
 import './index.css'
-import { createBrowserRouter, RouterProvider } from 'react-router-dom'
-import { AuthProvider } from './context/AuthProvider.jsx'
-import { createRoot } from 'react-dom/client'
-import { StrictMode } from 'react'
-import 'bootstrap/dist/css/bootstrap.min.css'
-import 'bootstrap/dist/js/bootstrap.bundle.min.js'
-import 'bootstrap-icons/font/bootstrap-icons.css'
-import '../src/assets/css/page-animations.css'
-import './index.css'
 
 import Layout from './components/Layout.jsx'
 import Login from './pages/auth/Login.jsx'
@@ -159,7 +150,6 @@ const router = createBrowserRouter([
   },
   { path: '*', element: <Error404 /> },
 ])
-
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
