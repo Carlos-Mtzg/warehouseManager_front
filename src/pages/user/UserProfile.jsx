d095dcd38a672ee87df8df202331079ea52e1b4a
Carlos-mtzg <carlos.mtzg2004@gmail.com>
2025-04-08 23:50:09 -0600

    feat(UserProfile): :sparkles: Enhance UserProfile component with improved layout and styling

--- src/pages/user/UserProfile.jsx
+++ src/pages/user/UserProfile.jsx
@@ -1,5 +1,7 @@
 import { getUserByUUID } from '../../services/ApiUser';
 import { useState, useEffect } from 'react';
+import styles from '../../assets/css/users.module.css'
+import profile from '../../assets/images/profile.png'
 
 const UserProfile = () => {
     const [user, setUser] = useState(null);
@@ -23,12 +25,45 @@ const UserProfile = () => {
     }
 
     return (
-        <div>
-            <h1>Mi Perfil</h1>
-            <p><strong>Nombre:</strong> {user.name}</p>
-            <p><strong>Apellido:</strong> {user.lastname}</p>
-            <p><strong>Correo Electrónico:</strong> {user.email}</p>
-            <p><strong>Rol:</strong> {user.role?.name === 'ROLE_ADMIN' ? 'Administrador' : 'Usuario'}</p>
+        <div className="d-flex flex-column gap-4">
+            <div className='bg-white rounded p-4 slide-up'>
+                <div className='col-12 col-md-8 d-flex gap-2'>
+                    <img className={`d-none d-lg-block ${styles['profile']}`} src={profile} alt="profile" />
+                    <div className="user-info">
+                        <p className={`mb-1 fs-1 fw-semibold ${styles['primary-text']}`}>{user.name} {user.lastname}</p>
+                        <p className={`mb-1 ${styles['secondary-text']}`}>{user.email}</p>
+                        <p className={`mb-1 ${styles['secondary-text']}`}>{user.role?.name === 'ROLE_ADMIN' ? 'Administrador' : 'Usuario'}</p>
+                    </div>
+                </div>
+            </div>
+            <div className='d-flex flex-column bg-white rounded p-4 gap-3 slide-up'>
+                <p className={`fw-bold text-uppercase fs-4 ${styles['title']}`}>Mi información</p>
+                <div className='row'>
+                    <div className="form-group col-12 col-md-4 mb-4">
+                        <label className={`fw-semibold ${styles['label']}`} htmlFor="name">Nombre:</label>
+                        <input className='form-control mt-2' type="text" readOnly value={user.name} />
+                    </div>
+                    <div className="form-group col-12 col-md-4 mb-4">
+                        <label className={`fw-semibold ${styles['label']}`} htmlFor="lastname">Apellido(s):</label>
+                        <input className='form-control mt-2' type="text" readOnly value={user.lastname} />
+                    </div>
+                    <div className="form-group col-12 col-md-4 mb-4">
+                        <label className={`fw-semibold ${styles['label']}`} htmlFor="email">Correo Electrónico:</label>
+                        <input className='form-control mt-2' type="text" readOnly value={user.email} />
+                    </div>
+                </div>
+                <div className="d-flex">
+                    <button
+                        className={`rounded ms-auto ${styles['primary-btn']}`}
+                        type='button'
+                    >
+                        <div className={`d-flex text-center py-1 px-2 ${styles['primary-content']}`}>
+                            Editar Información<i className="bi bi-pencil-square ms-2"></i>
+                        </div>
+                        <span></span>
+                    </button>
+                </div>
+            </div>
         </div>
     );
 };
